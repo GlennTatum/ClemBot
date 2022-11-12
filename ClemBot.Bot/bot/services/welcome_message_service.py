4756970b5595ccb92dbea6a2e1fc04ee10883d91
GlennTatum <glennetatum@gmail.com>
2022-11-12 17:29:09 -0500

    Info on users without Direct Messages enabled for a server

--- ClemBot.Bot/bot/services/welcome_message_service.py
+++ ClemBot.Bot/bot/services/welcome_message_service.py
@@ -16,6 +16,9 @@ class WelcomeMessageService(BaseService):
     async def user_joined(self, user: discord.Member) -> None:
         message = await self.bot.welcome_message_route.get_welcome_message(user.guild.id)
 
+        # TODO create try/except clause to handle users which have server "Direct Messages" disabled
+        # Traceback exception: https://discord.com/channels/386585461285715968/729492221904158781/1041112145166876682
+
         if message and not user.bot:
             await user.send(message)
 
